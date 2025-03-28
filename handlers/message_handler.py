e3e3d24891bdbd685e944e9bf9bd000d26edfd5b
AplustripleDev <momotiresthailand@gmail.com>
2025-03-28 17:11:19 +0700

    Update message_handler.py

--- handlers/message_handler.py
+++ handlers/message_handler.py
@@ -1,10 +1,9 @@
 import os
 import requests
 import re
+from dotenv import load_dotenv  # ✅ ต้องมีบรรทัดนี้
 from .sheet_handler import find_tire_stock
-from .ai_handler import ask_gpt  # ถ้าใช้ GPT
 from .ai_gemini import ask_gpt
-
 
 load_dotenv()
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
